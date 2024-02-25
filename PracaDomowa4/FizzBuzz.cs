83a2ad357e26f7a4d9da3e3895dd6dcda387c76f
Ewitkiewicz <kewitkiewicz@gmail.com>
2024-02-26 00:58:44 +0100

    Dodaj pliki projektów.

---
+++ PracaDomowa4/FizzBuzz.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracaDomowa4
+{
+    class FizzBuzz
+    {
+        public FizzBuzz() 
+        {
+        }
+
+        public string FizzBuzzMethod()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Podales nieprawidlowe dane");
+
+                }
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(number % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (number % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    return number.ToString();
+                };
+            }
+ 
+        }
+    }
+}
---
+++ PracaDomowa4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracaDomowa4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                Console.WriteLine("Wpisz liczbe: ");
+                FizzBuzz fizzBuzz = new FizzBuzz();
+                fizzBuzz.FizzBuzzMethod();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+
+    }
+}
